02ec1d14d897049a2efaeb92fc6e25fe178b9821
gulamhusen <gulamhusen@webmynesystems.com>
2017-07-14 11:00:23 +0530

    init

---
+++ app/src/main/java/com/webmyne/rentalapp/widget/TfRoundedEditText.java
@@ -0,0 +1,21 @@
+package com.webmyne.rentalapp.widget;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by gulamhusen on 14-07-2017.
+ */
+
+public class TfRoundedEditText extends LinearLayout {
+
+    public TfRoundedEditText(Context context) {
+        super(context);
+    }
+
+    public TfRoundedEditText(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
