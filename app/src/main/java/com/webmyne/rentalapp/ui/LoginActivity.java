16cbb6b9e7cb9411624d3a9dd7624ed63b1d965a
gulamhusen <gulamhusen@webmynesystems.com>
2017-07-17 11:51:55 +0530

    init

--- app/src/main/java/com/webmyne/rentalapp/ui/LoginActivity.java
+++ app/src/main/java/com/webmyne/rentalapp/ui/LoginActivity.java
@@ -2,35 +2,42 @@ package com.webmyne.rentalapp.ui;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.EditText;
 
 import com.webmyne.rentalapp.R;
-import com.webmyne.rentalapp.custom.TfButton;
 import com.webmyne.rentalapp.custom.TfTextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     private TfTextView txtRegister;
-    private TfButton btnSubmit;
     private Context context;
+    private EditText edtPassword;
+    private boolean passwordPress = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         init();
     }
 
     private void init() {
         context = this;
         txtRegister = (TfTextView) findViewById(R.id.txtRegister);
-        btnSubmit = (TfButton) findViewById(R.id.btnSubmit);
+        edtPassword = (EditText) findViewById(R.id.edtPassword);
         clickListener();
     }
 
     private void clickListener() {
+
         txtRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,13 +45,36 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
+
+        edtPassword.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
-                Intent i = new Intent(LoginActivity.this, DashboardActivity.class);
-                startActivity(i);
-                finish();
+            public boolean onTouch(View v, MotionEvent event) {
+
+                final int DRAWABLE_LEFT = 0;
+                final int DRAWABLE_TOP = 1;
+                final int DRAWABLE_RIGHT = 2;
+                final int DRAWABLE_BOTTOM = 3;
+
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    if (event.getRawX() >= (edtPassword.getRight() - edtPassword.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+
+                        if (passwordPress) {
+                            edtPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                            edtPassword.setCompoundDrawablesWithIntrinsicBounds(null, null, ContextCompat.getDrawable(LoginActivity.this, R.drawable.eye_black), null);
+                            edtPassword.setSelection(edtPassword.length());
+                            passwordPress = false;
+                        } else {
+                            edtPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                            edtPassword.setCompoundDrawablesWithIntrinsicBounds(null, null, ContextCompat.getDrawable(LoginActivity.this, R.drawable.eye_red), null);
+                            passwordPress = true;
+                            edtPassword.setSelection(edtPassword.length());
+                        }
+                        return true;
+                    }
+                }
+                return false;
             }
         });
     }
+
 }
